92b59d474672d923d0ea071affb33b403d362b2a
Hoang Tran <tdll.trdphoang@gmail.com>
2022-12-16 00:23:44 -0800

    Api filtering on Products Page
    Finished filtering items by:
    + Product categories
    + Price
    + Price (lowest first / highest first)

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,28 +2,46 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import List from "../../components/List/List";
 import "./Products.scss";
+import useFetch from "../../hooks/useFetch";
 
 const Products = () => {
 	const catId = parseInt(useParams().id);
+	const [priceValue, setPriceValue] = useState(0);
 	const [maxPrice, setMaxPrice] = useState(1000);
 	const [sort, setSort] = useState(null);
+	const [selectedSubCats, setSelectedSubCats] = useState([]);
+
+	const { data, loading, error } = useFetch(
+		`/sub-categories?[filters][categories][id][$eq]=${catId}`
+	);
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const isChecked = e.target.checked;
+
+		setSelectedSubCats(
+			isChecked
+				? [...selectedSubCats, value]
+				: selectedSubCats.filter((item) => item !== value)
+		);
+	};
+
 	return (
 		<div className="products">
 			<div className="left">
 				<div className="filterItem">
 					<h2>Product Categories</h2>
-					<div className="inputItem">
-						<input type="checkbox" id="1" value={1} />
-						<label htmlFor="1">Shoes</label>
-					</div>
-					<div className="inputItem">
-						<input type="checkbox" id="2" value={2} />
-						<label htmlFor="2">Skirts</label>
-					</div>
-					<div className="inputItem">
-						<input type="checkbox" id="3" value={3} />
-						<label htmlFor="3">Coats</label>
-					</div>
+					{data?.map((item) => (
+						<div className="inputItem" key={item.id}>
+							<input
+								type="checkbox"
+								id="1"
+								value={item.id}
+								onChange={handleChange}
+							/>
+							<label htmlFor={item.id}>{item.attributes.title}</label>
+						</div>
+					))}
 				</div>
 				<div className="filterItem">
 					<h2>Filter by price</h2>
@@ -33,9 +51,12 @@ const Products = () => {
 							type="range"
 							min={0}
 							max={1000}
-							onChange={(e) => setMaxPrice(e.target.value)}
+							onChange={(e) => setPriceValue(e.target.value)}
 						/>
-						<span>{maxPrice}</span>
+						<span>{priceValue}</span>
+						<button id="applyPriceBtn" onClick={() => setMaxPrice(priceValue)}>
+							Apply
+						</button>
 					</div>
 				</div>
 				<div className="filterItem">
@@ -68,7 +89,12 @@ const Products = () => {
 					src="https://cdn-fsly.yottaa.net/5e18d625d9314057054ee33e/www.ralphlauren.com/v~4b.88/on/demandware.static/-/Sites-RalphLauren_US-Library/default/dwfc3c1ff7/img/202210/20221018-holiday-shared-in-grid/1018_holiday_plp_shared_wishlist.jpg?yocs=2M_2Q_2T_"
 					alt=""
 				/>
-				<List catId={catId} maxPrice={maxPrice} sort={sort} />
+				<List
+					catId={catId}
+					maxPrice={maxPrice}
+					sort={sort}
+					subCats={selectedSubCats}
+				/>
 			</div>
 		</div>
 	);
